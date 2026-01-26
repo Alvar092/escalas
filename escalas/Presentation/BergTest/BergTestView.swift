3328fde2d494d05d0c3e721c9c7279db64900be5
Álvaro Entrena Casas <aentrena@MacBook-Air-de-Alvaro.local>
2026-01-26 17:13:52 +0100

    refactor(bergTestView): reorgnize layout with ZStack and ScrollView

--- escalas/Presentation/BergTest/BergTestView.swift
+++ escalas/Presentation/BergTest/BergTestView.swift
@@ -14,113 +14,124 @@ struct BergTestView: View {
     
     
     var body: some View {
-        VStack(spacing: 24) {
-            
-            VStack(alignment:.center, spacing: 8) {
-                Text(viewModel.progress)
-                    .font(.headline)
-                
-                HStack(alignment: .center) {
-                    Text(viewModel.currentItemDefinition.title)
-                        .font(.title2)
-                        .bold()
-                   
-                }
-                
-                Text(viewModel.currentItemDefinition.description)
-            } //VStack Pregunta y marcador
-            
-            if viewModel.currentItemDefinition.needsTimer {
-                VStack {
-                    Text(viewModel.formattedTime)
-                        .font(.system(size: 48, weight: .bold, design: .monospaced))
-                }
-                
-                HStack(spacing: 32) {
-                    Button(viewModel.isTimerRunning ? "Pausar" : "Iniciar"){
-                        if viewModel.isTimerRunning {
-                            viewModel.stopTimer()
-                        } else {
-                            viewModel.startTimer()
-                        }
-                    }
+        ZStack(alignment: .bottom){
+            ScrollView {
+                VStack(spacing: 24) {
+                    VStack(alignment:.leading, spacing: 16) {
+                        Text(viewModel.progress)
+                            .font(.headline)
+                        
+                        Text(viewModel.currentItemDefinition.title)
+                            .font(.title2)
+                            .bold()
+                        
+                        Text(viewModel.currentItemDefinition.description)
+                            .multilineTextAlignment(.leading)
+                    } //VStack Pregunta y marcador
                     
-                    Button("Reset") {
-                        viewModel.resetTimer()
-                    }
-                }
-                
-            } // Cronómetro
-            
-            Spacer()
-            VStack {
-                ForEach(viewModel.scoreOptions) { option in
-                    Button {
-                        viewModel.selectScore(option.score)
-                    } label: {
-                        HStack{
-                            Text(option.description)
-                                .foregroundStyle(.primary)
+                    
+                    if viewModel.currentItemDefinition.needsTimer {
+                        HStack(spacing: 16) {
+                            Text(viewModel.formattedTime)
+                                .font(.system(size: 28, weight: .bold, design: .monospaced))
                             
                             Spacer()
+                            
+                            Button(viewModel.isTimerRunning ? "Pausar" : "Iniciar"){
+                                if viewModel.isTimerRunning {
+                                    viewModel.stopTimer()
+                                } else {
+                                    viewModel.startTimer()
+                                }
+                            }
+                            Button("Reset") {
+                                viewModel.resetTimer()
+                            }
                         }
-                        .padding(16)
-                        
-                        .background(                            RoundedRectangle(cornerRadius: 8)
-                                .fill(
-                                    viewModel.isOptionSelected(option)
-                                    ? Color.accentColor.opacity(0.2) : Color.clear
+                        .padding(.horizontal, 32)
+                    } // Cronómetro
+                    
+                    VStack(spacing: 12) {
+                        ForEach(viewModel.scoreOptions) { option in
+                            Button {
+                                viewModel.selectScore(option.score)
+                            } label: {
+                                HStack(alignment: .top){
+                                    Text(option.description)
+                                        .foregroundStyle(.primary)
+                                        .font(.subheadline)
+                                        .multilineTextAlignment(.leading)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                    
+                                    Spacer()
+                                }
+                                .padding(.horizontal, 12)
+                                .padding(.vertical, 16)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(RoundedRectangle(cornerRadius: 8)
+                                    .fill(
+                                        viewModel.isOptionSelected(option)
+                                        ? Color.accentColor.opacity(0.2) : Color.clear
+                                    )
                                 )
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(
-                                    viewModel.isOptionSelected(option)
-                                    ? Color.blue
-                                    : Color.blue.opacity(0.7)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(
+                                            viewModel.isOptionSelected(option)
+                                            ? Color.blue
+                                            : Color.blue.opacity(0.7)
+                                        )
                                 )
-                            )
-                    }
-                }
-            } //VStack Opciones respuestas
-            
-            Spacer()
-            
-            HStack(alignment: .bottom) {
-                Button{
-                    viewModel.backItem()
-                } label: {
-                    Text("Atras")
-                }
-                
-                Spacer()
-                
-                VStack{
-                    Text("Puntuación\ntotal: \(viewModel.totalScore)/56")
-                        .font(.headline)
-                }
-                Spacer()
-                
-                Button(viewModel.isLastItem ? "Finalizar" : "Siguiente") {
-                    if viewModel.isLastItem {
-                        Task {
-                            try? await viewModel.finishTest()
+                            }
                         }
-                    } else {
-                        viewModel.nextItem()
-                    }
-                }
+                        .buttonStyle(.plain)
+                    } //VStack Opciones respuestas
+                }//VStack padre
+                .padding()
+                .padding(.bottom, 100)
             }
-            .padding()
-            
-        }//VStack padre
-        .padding()
+            bottomBar
+        }
         .navigationDestination(isPresented: $viewModel.navigateToResultView) {
             ScaleResultView(viewModel: ScaleResultViewModel(test: viewModel.test, useCase: GetPatientByIdUseCase(patientsRepository: repositories.patientRepository)))
         }
     }
+    
+    private var bottomBar: some View {
+        HStack(alignment: .bottom) {
+            Button{
+                viewModel.backItem()
+            } label: {
+                Text("Atras")
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .center){
+                Text("Puntuación\ntotal: \(viewModel.totalScore)/56")
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            
+            Button(viewModel.isLastItem ? "Finalizar" : "Siguiente") {
+                if viewModel.isLastItem {
+                    Task {
+                        try? await viewModel.finishTest()
+                    }
+                } else {
+                    viewModel.nextItem()
+                }
+            }
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .ignoresSafeArea(edges: .bottom)
+    }
+    
+    
+    
 }
-
 
 #Preview {
     let mockRepository = MockBergTestRepository()
@@ -129,3 +140,4 @@ struct BergTestView: View {
         useCase: SaveBergTestUseCase(repository: mockRepository),
         test: BergTest.patient1))
 }
+
